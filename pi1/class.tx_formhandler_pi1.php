8a5b374ee0065c0c7504dce530eb6cda8e452c5c
Felix Kopp <felix-source@phorax.com>
2017-08-05 11:51:08 +0200

    [!!!][TASK] Remove [TYPO3_MODE]['XCLASS’] for pi1
    Removes former XLASS registration

--- pi1/class.tx_formhandler_pi1.php
+++ pi1/class.tx_formhandler_pi1.php
@@ -46,7 +46,3 @@ class tx_formhandler_pi1 extends TYPO3\CMS\Frontend\Plugin\AbstractPlugin
         return $dispatcher->main($content, $conf);
     }
 }
-
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/formhandler/pi1/class.tx_formhandler_pi1.php']) {
-    include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/formhandler/pi1/class.tx_formhandler_pi1.php']);
-}
