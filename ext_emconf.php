7db04c82d39ce9b8b1b02f8ba4b6122f17d0ee66
Felix Kopp <felix-source@phorax.com>
2018-11-23 06:36:25 +0100

    [TASK] Adapt ext_emconf to composer settings

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
   'author_company' => 'Typoheads GmbH',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '7.6.0-8.7.99',
+      'typo3' => '9.5.0-9.5.99',
       'typo3db_legacy' => ''
     ),
   ),
