1512548fef5b638ebb75624ea7abf8e1d4b6c526
Felix Kopp <felix-source@phorax.com>
2020-05-14 19:15:39 +0200

    [TASK] Complete Extbase persistence definition

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -1,21 +1,39 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 return [
     \Typoheads\Formhandler\Domain\Model\LogData::class => [
         'tableName' => 'tx_formhandler_log',
         'properties' => [
+            'crdate' => [
+                'fieldname' => 'crdate'
+            ],
             'isSpam' => [
-                'fieldName' => 'is_spam'
-            ]
-        ]
+                'fieldname' => 'isSpam'
+            ],
+            'params' => [
+                'fieldname' => 'params'
+            ],
+            'ip' => [
+                'fieldname' => 'ip'
+            ],
+        ],
     ],
     \Typoheads\Formhandler\Domain\Model\Demand::class => [
-        'tableName' => 'tx_formhandler_log',
+        'tableName' => 'static_country_zones',
         'properties' => [
+            'crdate' => [
+                'fieldname' => 'crdate'
+            ],
             'isSpam' => [
-                'fieldName' => 'is_spam'
-            ]
+                'fieldname' => 'isSpam'
+            ],
+            'params' => [
+                'fieldname' => 'params'
+            ],
+            'ip' => [
+                'fieldname' => 'ip'
+            ],
         ]
     ]
 ];
