2d3ce8c67750ba6a5a4006968861704eabaaca41
Felix Kopp <felix-source@phorax.com>
2020-05-14 19:13:17 +0200

    [TASK] Raise minimum requirement to 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
   'author_company' => 'Typoheads GmbH',
   'constraints' => [
     'depends' => [
-      'typo3' => '9.5.0-9.5.99',
+      'typo3' => '10.4.0-10.4.99',
     ],
   ],
   'uploadfolder' => false,
