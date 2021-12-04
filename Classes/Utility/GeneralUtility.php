8a47ffee5bb55691c62a8a70eadcdddfe6fec5b1
Thomas Lüder <t.lueder@nets.de>
2021-12-04 23:26:02 +0100

    Make sure additionalParams is an array

--- Classes/Utility/GeneralUtility.php
+++ Classes/Utility/GeneralUtility.php
@@ -379,7 +379,7 @@ class GeneralUtility implements SingletonInterface
                     $additionalParams[$key] = $value;
                 }
             } else {
-                $additionalParams = $settings['additionalParams.'];
+                $additionalParams = (array)$settings['additionalParams.'];
             }
             self::doRedirect($redirectPage, $correctRedirectUrl, $additionalParams, $headerStatusCode);
             exit();
