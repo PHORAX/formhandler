b4437352eebbdafa8c4cc7966287dfdd015d52b3
Michael Krohn <krohn@jakota.de>
2022-03-23 17:16:18 +0100

    Add todo

--- Classes/PreProcessor/LoadDefaultValues.php
+++ Classes/PreProcessor/LoadDefaultValues.php
@@ -83,6 +83,8 @@ class LoadDefaultValues extends AbstractPreProcessor
     {
         if (is_array($settings) && $step) {
             $values = (array)$this->globals->getSession()->get('values');
+            // TODO Error with default values at step 2
+
             $this->setDefaultValues($settings, $values[$step]);
             $this->globals->getSession()->set('values', $values);
         }
