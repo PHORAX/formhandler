1635a66af43cd08e310d4369eb13460e06ec655f
Thomas Lüder <t.lueder@nets.de>
2022-04-08 13:04:40 +0200

    Fixed return type

--- Classes/Generator/TcPdf.php
+++ Classes/Generator/TcPdf.php
@@ -78,7 +78,7 @@ class TcPdf extends AbstractGenerator {
 
       $downloadpath = $this->filename;
       if ($returns) {
-        return $downloadpath;
+        return [$downloadpath];
       }
       $downloadpath = str_replace($this->utilityFuncs->getDocumentRoot(), '', $downloadpath);
       header('Location: '.$downloadpath);
